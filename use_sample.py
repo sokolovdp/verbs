bbf8206873817114ce53331130d01407dfe828bd
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-19 12:47:14 +0300

    improve readme

--- use_sample.py
+++ use_sample.py
@@ -2,6 +2,5 @@ import sys
 from verbs_usage import find_verbs_in_python_code
 
 
-if __name__ == '__main__':
-    python_project_directory = sys.argv[1]
-    find_verbs_in_python_code(path_name=python_project_directory, max_common=5)
+python_project_directory = sys.argv[1]
+find_verbs_in_python_code(path_name=python_project_directory, max_common=5)
